afc81c892e1ecf6bbddae832acaea473c34c8dde
甜虾 <daizhen.dz@alibaba-inc.com>
2018-05-11 10:13:03 +0800

    评论

--- docs/.vuepress/components/CommentTool.vue
+++ docs/.vuepress/components/CommentTool.vue
@@ -5,16 +5,17 @@
 <script>
 export default {
 	mounted () {
-		let title = document.location
-		let gitment = new Gitment({
-		    id: title,
-		    owner: 'xiaotianxia',
-		    repo: 'blog',
-		    oauth: {
-		      	client_id: 'a57e9245872c968a247c',
-		      	client_secret: '537db1f2fe58008a9dabfbb7b1cb1a33e02da462',
-		    }
-		  })
+		let urls = document.location.href.split('/'),
+			id = urls[urls.length - 1],
+			gitment = new Gitment({
+			    id: id,
+			    owner: 'xiaotianxia',
+			    repo: 'blog',
+			    oauth: {
+			      	client_id: 'a57e9245872c968a247c',
+			      	client_secret: '537db1f2fe58008a9dabfbb7b1cb1a33e02da462',
+			    }
+			  })
 	  	gitment.render('comment-container')
 	}
 }
