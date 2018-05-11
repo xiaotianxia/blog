9562f0fb12afbd0d57e3e12b4f5e7c3145eed5f7
甜虾 <daizhen.dz@alibaba-inc.com>
2018-05-11 16:47:53 +0800

    改文件名

--- docs/.vuepress/titles/js.js
+++ docs/.vuepress/titles/js.js
@@ -3,6 +3,6 @@ module.exports = [
 	'shape-detection-api-trial',
 	'simple_chatroom_using_socketIO',
 	'communication_using_postMessage',
-	'upload_using_fileReader_and_createObjectURL',
+	'upload_using_filereader_createobjecturl',
 	'simple_code_preview'
 ]
