2fb71a4afc87cf9342928307fd6b431337ebd321
甜虾 <daizhen.dz@alibaba-inc.com>
2018-05-15 22:53:53 +0800

    优化

--- docs/.vuepress/components/Speech/Synthesis.vue
+++ docs/.vuepress/components/Speech/Synthesis.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="speech-wrapper">
+    <div class="speech-wrapper" element-loading-text="加载语言包..." v-loading="!voiceData.length">
         <el-form v-model="queryParams" label-width="90px" label-position="left">
+            <el-form-item label="文字:">
+                <el-input v-model="queryParams.text" type="textarea" :autosize="{ minRows: 2, maxRows: 6}"></el-input>
+            </el-form-item>
+
             <el-form-item label="选择语言:">
                 <el-select v-model="queryParams.lang" @change="onChange">
                     <el-option v-for="item in voiceData" :key="item.voiceURI" :value="item.lang" :label="item.lang"></el-option>
@@ -19,12 +23,10 @@
                 <el-slider v-model="queryParams.pitch" :min="0" :max="2" :step="0.2" show-input></el-slider>
             </el-form-item>
 
-            <el-form-item label="文字:">
-                <el-input v-model="queryParams.text" clearable></el-input>
-            </el-form-item>
-
             <el-form-item>
-                <el-button type="primary" @click="onSpeak" size="small">朗读</el-button>
+                <el-tooltip class="item" effect="dark" content="接口不稳定，有可能没有声音，刷新几次试试？" placement="top">
+                    <el-button type="primary" @click="onSpeak" size="small">朗读</el-button>
+                </el-tooltip>
             </el-form-item>
         </el-form>
     </div>
