55b4cf9e9cb493c85d87b42553d8911ae89ec32c
甜虾 <daizhen.dz@alibaba-inc.com>
2018-05-15 14:49:04 +0800

    猫

---
+++ docs/.vuepress/components/ImageSlider.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<el-carousel :interval="4000" type="card" height="400px">
+		    <el-carousel-item v-for="(item, key) in srcData" :key="key">
+		      	<img :src="item">
+		    </el-carousel-item>
+		</el-carousel>
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {}
+	},
+
+	props: [
+		'srcData'
+	]
+}
+</script>
+
+<style>
+
+</style>
