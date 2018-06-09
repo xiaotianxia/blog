07ed276b63e7bd69fae3aaac70288a997721056d
甜虾 <daizhen.dz@alibaba-inc.com>
2018-06-09 23:14:33 +0800

    not

--- docs/.vuepress/components/Notification/Demo.vue
+++ docs/.vuepress/components/Notification/Demo.vue
@@ -1,6 +1,47 @@
 <template>
 	<div class="notification-wrapper">
-		222
+		<el-form v-model="queryParams" label-width="90px" label-position="left">
+            <el-form-item label="title:">
+                <el-input v-model="queryParams.title" placeholder="题目..."></el-input>
+            </el-form-item>
+
+            <el-form-item label="body:">
+            	<el-input v-model="queryParams.body" type="textarea" :autosize="{ minRows: 2, maxRows: 6}" placeholder="内容..."></el-input>
+                
+            </el-form-item>
+
+            <el-form-item label="icon:">
+                <el-input v-model="queryParams.icon" placeholder="图标"></el-input>
+            </el-form-item>
+
+            <el-form-item label="icon预览:">
+                <img :src="queryParams.icon" alt="">
+            </el-form-item>
+
+            <el-form-item label="image:">
+                <el-input v-model="queryParams.image" placeholder="图标"></el-input>
+            </el-form-item>
+
+            <el-form-item label="image预览:">
+                <img :src="queryParams.image" alt="">
+            </el-form-item>
+
+            <el-form-item label="sound:">
+                <el-input v-model="queryParams.sound" placeholder="图标"></el-input>
+            </el-form-item>
+
+            <el-form-item label="sound预览:">
+                <audio :src="queryParams.sound" controls="controls"></audio>
+            </el-form-item>
+
+            <el-form-item>
+                <el-button type="primary" @click="onConfirm" size="small">通知一下~</el-button>
+            </el-form-item>
+
+            <el-form-item>
+                <span>携带data:{{time}}</span>
+            </el-form-item>
+        </el-form>
 	</div>
 </template>
 
@@ -8,24 +49,69 @@
 export default {
 	data () {
 		return {
-			
+			queryParams: {
+				title: '一条新通知',
+				body: '12345哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈12345',
+				icon: 'https://denzel.netlify.com/hero.png',
+				badge: 'https://denzel.netlify.com/hero.png',
+				image: 'https://denzel.netlify.com/hero.png',
+				// silent: true,
+				sound: 'http://p8rbt50i2.bkt.clouddn.com/blogsmile.wav',
+				dir: 'rtl',
+				data: {
+					t: new Date()
+				},
+				vibrate: []
+            },
+            time: ''
 		}
 	},
 
-	computed: {
-		
-	},
-
 	methods: {
-		
-	},
+		onConfirm () {
+			if (!('Notification' in window)) {
+			    alert('您的浏览器不支持通知API');
+			}
+			if (Notification.permission == "granted") {
+	            this.newNotification();
+	        } else if (Notification.permission != "denied") {
+	            Notification.requestPermission(function (permission) {
+	              	this.newNotification();
+	            });
+	        }
+		},
 
-	mounted () {
-		
+		newNotification () {
+			let notification = new Notification(this.queryParams.title, {
+				...this.queryParams
+			});
+
+			console.log(notification);
+
+			notification.addEventListener('show', e => {
+				console.log(e);
+				this.$message.info('通知出现');
+				this.time = notification.data.t.toLocaleTimeString();
+			});
+
+
+			notification.addEventListener('click', e => {
+				console.log(e);
+				this.$message.info('你点击了通知');
+				// notification.close();
+			});
+
+			notification.addEventListener('close', e => {
+				console.log(e);
+				this.$message.info('你关闭了通知');
+			});
+		}
 	}
 }
 </script>
 
 <style scoped>
-	
+	.notification-wrapper {
+		margin-top: 20px;
+	}
 </style>
