f6206885598a2bbbd58d4751c5d3ffc1ad7e0609
甜虾 <daizhen.dz@alibaba-inc.com>
2018-08-20 15:20:39 +0800

    canvas

--- docs/.vuepress/components/Canvas/VideoCutout.vue
+++ docs/.vuepress/components/Canvas/VideoCutout.vue
@@ -43,7 +43,8 @@ export default {
         this.canvas = this.$refs['canvas'];
         this.ctx = this.canvas.getContext('2d');
 
-        this.video.addEventListener('play', () => {
+        this.video.addEventListener('playing', () => {
+        	console.log('playing')
             this.width = this.video.videoWidth;
             this.height = this.video.videoHeight;
             this.draw();
