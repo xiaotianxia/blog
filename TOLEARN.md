e21cb97e03e6278ad9234e4a7a19d87fa6177c62
甜虾 <daizhen.dz@alibaba-inc.com>
2019-03-18 16:22:41 +0800

    mutation

--- TOLEARN.md
+++ TOLEARN.md
@@ -1,12 +1,8 @@
-闭包
-调用栈
-Promise
-ES6
-工程化
+
 webpack 运行机制
 脚手架
-安全问题
-Virtual DOM
-Diff 算法
-移动端布局
-浏览器渲染原理
+
+
+
+
+
--- docs/.vuepress/components/Observers/IntersectionObserver.vue
+++ docs/.vuepress/components/Observers/IntersectionObserver.vue
@@ -13,6 +13,9 @@
 						<div class="video js-video hide">
 							<video :src="item.url" muted @click="onClickVideo"></video>
 						</div>
+                        <div class="description">
+                            <p style="-webkit-box-orient: vertical;">{{item.description}}</p>
+                        </div>
 					</li>
 				</ul>
 			</div>
@@ -25,13 +28,13 @@ export default {
 	data () {
 		return {
 			list: [
-				{cover: 'https://wx1.sinaimg.cn/large/764d0c29ly1fvrk7lg8nij20g80dcmxr.jpg', url: 'http://pn4meizzc.bkt.clouddn.com/video.ogv'},
-				{cover: 'https://wx1.sinaimg.cn/large/74cd34adly1fvogc7wyf3j20dc0dct9g.jpg', url: 'http://pn4meizzc.bkt.clouddn.com/video.ogv'},
-				{cover: 'https://wx1.sinaimg.cn/large/764d0c29ly1fvrk7lg8nij20g80dcmxr.jpg', url: 'http://pn4meizzc.bkt.clouddn.com/video.ogv'},
-				{cover: 'https://wx1.sinaimg.cn/large/74cd34adly1fvogc7wyf3j20dc0dct9g.jpg', url: 'http://pn4meizzc.bkt.clouddn.com/video.ogv'},
-				{cover: 'https://wx1.sinaimg.cn/large/74cd34adly1fvogc7wyf3j20dc0dct9g.jpg', url: 'http://pn4meizzc.bkt.clouddn.com/video.ogv'},
-				{cover: 'https://wx1.sinaimg.cn/large/74cd34adly1fvogc7wyf3j20dc0dct9g.jpg', url: 'http://pn4meizzc.bkt.clouddn.com/video.ogv'},
-				{cover: 'https://wx1.sinaimg.cn/large/74cd34adly1fvogc7wyf3j20dc0dct9g.jpg', url: 'http://pn4meizzc.bkt.clouddn.com/video.ogv'},
+				{cover: 'http://poijlzkaq.bkt.clouddn.com/3213214321.png', url: 'http://poijlzkaq.bkt.clouddn.com/1552895951232209.mp4', description: '大花猫呀大花猫呀大花猫呀大花猫呀大花猫呀大花猫呀大花猫呀大花猫呀大花猫呀大花猫呀大花猫呀大花猫呀大花猫呀大花猫大花猫呀大花猫大花猫呀大花猫大花猫呀大花猫大花猫呀大花猫大花猫呀大花猫大花猫呀大花猫'},
+				{cover: 'http://poijlzkaq.bkt.clouddn.com/143124321.png', url: 'http://poijlzkaq.bkt.clouddn.com/1552895945898993.mp4', description: '小乌龟啊小乌龟啊小乌龟啊小乌龟啊小乌龟啊小乌龟啊小乌龟啊小乌龟啊小乌龟啊小乌龟啊小乌龟啊小乌龟小乌龟啊小乌龟小乌龟啊小乌龟小乌龟啊小乌龟小乌龟啊小乌龟小乌龟啊小乌龟'},
+				{cover: 'http://poijlzkaq.bkt.clouddn.com/586337357.png', url: 'http://poijlzkaq.bkt.clouddn.com/1552896499270164.mp4', description: '哈哈哈哈哈哈哈哈哈哈'},
+				{cover: 'http://poijlzkaq.bkt.clouddn.com/678976986798.png', url: 'http://poijlzkaq.bkt.clouddn.com/1552896500269933.mp4', description: '嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿'},
+				{cover: 'http://poijlzkaq.bkt.clouddn.com/6095487609.png', url: 'http://poijlzkaq.bkt.clouddn.com/1552896502100868.mp4', description: '呵呵呵呵呵呵呵呵'},
+				{cover: 'http://poijlzkaq.bkt.clouddn.com/48666754765.png', url: 'http://poijlzkaq.bkt.clouddn.com/1552896503859343.mp4', description: '哈哈哈哈哈哈哈哈哈哈'},
+				{cover: 'http://poijlzkaq.bkt.clouddn.com/434689743867.png', url: 'http://poijlzkaq.bkt.clouddn.com/1552896506605984.mp4', description: '哈哈哈哈哈哈哈哈哈哈'},
 			],
 			errorMsg: ''
 		}
@@ -150,6 +153,7 @@ export default {
 		padding: 40px 0;
 		overflow: auto;
 		overscroll-behavior: contain;
+        background-color: #000;
 	}
 	.wrapper .sectionContent::-webkit-scrollbar {
         display: none;
@@ -157,7 +161,7 @@ export default {
 	.wrapper .list .item {
 		position: relative;
 		height: 280px;
-		padding: 20px 0;
+		/* padding: 20px 0; */
 		overflow: hidden;
 	}
     .wrapper .list .item:not(:last-child) {
@@ -178,8 +182,7 @@ export default {
 		height: 30px;
 		line-height: 30px;
 		padding: 0 10px;
-		background-color: crimson;
-		border-radius: 8px;
+		background-color: rgb(237, 20, 60, .4);
 		color: #fff;
 		z-index: 10;
 	}
@@ -217,4 +220,24 @@ export default {
 		font-size: 4em;
 		color: #fff;
     }
+    .description {
+        position: absolute;
+        bottom: 0;
+        color: #fff;
+        background-color: rgb(0, 0, 0, .4);
+        width: 100%;
+        padding: 5px;
+        z-index: 9;
+        font-size: 12px;
+        text-align: left;
+        box-sizing: border-box;
+    }
+    .description p {
+        padding: 0;
+        margin: 0;
+        overflow: hidden;
+        display: -webkit-box;
+        
+        -webkit-line-clamp: 2;
+        }
 </style>
