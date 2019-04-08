a8218467d9f451247ec825775f2a7daa1c05c0cc
甜虾 <daizhen.dz@alibaba-inc.com>
2019-04-08 16:15:43 +0800

    xy

---
+++ docs/.vuepress/components/Event/XY.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="event-wrapper">
+        <div class="play-board">
+            offsetX: <span>{{result.offsetX}}</span>
+            offsetY: <span>{{result.offsetY}}</span><br>
+
+            clientX: <span>{{result.clientX}}</span>            
+            clientY: <span>{{result.clientY}}</span><br>
+
+            pageX: <span>{{result.pageX}}</span>            
+            pageY: <span>{{result.pageY}}</span><br>
+
+            screenX: <span>{{result.screenX}}</span>            
+            screenY: <span>{{result.screenY}}</span><br>
+
+            layerX: <span>{{result.layerX}}</span>            
+            layerY: <span>{{result.layerY}}</span><br>
+
+            x: <span>{{result.x}}</span>            
+            y: <span>{{result.y}}</span><br>
+
+            movementX: <span>{{result.movementX}}</span>            
+            movementY: <span>{{result.movementY}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            result: {},
+            timer: null
+        }
+    },
+
+    methods: {
+        doLog (e) {
+            this.timer && clearTimeout(this.timer);
+            this.timer = setTimeout(() => {
+                this.result = e;
+            }, 10);
+        }
+    },
+
+    mounted () {
+        document.addEventListener('mousemove', e => {
+            this.doLog(e);
+        });
+    }
+}
+</script>
+
+<style scoped>
+    .event-wrapper {
+        position: relative;
+        height: 500px;
+        border: 1px solid #000;
+    }
+    .play-board {
+        position: absolute;
+        left: 0;
+        top: 0;
+        padding: 10px;
+        width: 200px;
+        height: 100px;
+        color: #fff;
+        background-color: rgba(44, 62, 80, .7);
+        font-size: 12px;
+    }
+    .play-board span {
+        color: gold;
+        font-weight: bold;
+        margin-right: 10px;
+    }
+</style>
+
+
---
+++ docs/js/kinds_of_xs_and_ys.md
@@ -0,0 +1,5 @@
+## 11
+
+<Event-XY/>
+
+https://blog.csdn.net/lzding/article/details/45437707
